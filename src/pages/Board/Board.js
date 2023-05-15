c1cad5aa1ad084a0143106d2fe62b1fa3403a338
Wickhye <jihyesim170@gmail.com>
2023-05-16 08:12:10 +0900

    FEAT :: 게시판 리스트 데이터 받아옴~~

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,36 +1,41 @@
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { getboard } from "../../utils/api/board";
 import { useParams } from "react-router-dom";
+import { setBoard } from "../../utils/redux/board";
 
 const Board = () => {
   const { id } = useParams();
-  const [boards, setBoards] = useState([]);
+  const dispatch = useDispatch();
+  const board = useSelector((state) => state.boardsReducer.board);
 
   function initialBoards() {
     const myPromise = getboard(id);
-    // myPromise
-    //   .then((response) => {
-    //     const data = response.data;
-    //     setBoards(data, boards);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+    myPromise
+      .then(function (data) {
+        dispatch(setBoard(data.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   useEffect(() => {
     initialBoards();
-    console.log(id);
+    console.log(board);
   }, []);
 
   return (
     <div>
       <h1>라라라라</h1>
-      {boards &&
-        boards.map((board) => (
-          <>
-            <h1>아아아아아</h1>
-          </>
+      {board &&
+        board.map((board) => (
+          <div>
+            <h1>{board.title}</h1>
+            <span>{board.views}</span>
+            <p>{board.created}</p>
+            <p>{board.userNickname}</p>
+          </div>
         ))}
     </div>
   );
