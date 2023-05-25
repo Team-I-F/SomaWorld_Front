ca1667640460ec4d2d21d6d3ca335eeed6497ef7
Wickhye <jihyesim170@gmail.com>
2023-05-25 17:15:24 +0900

    CHORE :: readme 수정

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import * as S from "./style";
+
+function Footer() {
+  return (
+    <div>
+      <div>부산소마고의 유일한 상남자들</div>
+      <div>I-F</div>
+      <div>copyright in 2023</div>
+    </div>
+  );
+}
+
+export default Footer;
