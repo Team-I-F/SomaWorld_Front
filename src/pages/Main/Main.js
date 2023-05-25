836ba3e2b1945b2c7dcf3fa564703d5ebadb7d20
Wickhye <jihyesim170@gmail.com>
2023-05-25 16:59:41 +0900

    CHORE :: 메인 경로 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header";
 import Post from "../../components/Post/Post";
+import * as S from "./style";
 
 export default function Main() {
   return (
     <div>
       <Header />
       <div>
+        <S.Head>🔥인기글</S.Head>
         <Post />
       </div>
     </div>
---
+++ src/pages/Main/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Head = styled.div`
+  width: 128px;
+  height: 39px;
+  top: 341px;
+  left: 365px;
+  font-size: 25px;
+`;
