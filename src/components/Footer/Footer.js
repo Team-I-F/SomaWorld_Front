072855a21715a542047ce3f644b0479ffd3ca3de
Wickhye <jihyesim170@gmail.com>
2023-05-25 18:44:21 +0900

    ADD :: Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import * as S from "./style";
+
+function Footer() {
+  return (
+    <S.Footer>
+      <S.LeftText>
+        <div>부산소마고의 유일한 상남자들</div>
+        <div>@I-F</div>
+      </S.LeftText>
+      <S.LightText>copyright in 2023</S.LightText>
+    </S.Footer>
+  );
+}
+
+export default Footer;
