5836fd247a7f252b87f6f2a32e9f5fc538fabb2a
cheonnayeong <nayoung109191@gmail.com>
2023-06-01 20:20:17 +0900

    STYLE :: main페이지 frame 잡음

--- src/pages/Main/style.js
+++ src/pages/Main/style.js
@@ -5,5 +5,11 @@ export const Head = styled.div`
   height: 39px;
   top: 341px;
   left: 365px;
-  font-size: 25px;
+  font-size: 35px;
+`;
+
+export const posts = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
 `;
