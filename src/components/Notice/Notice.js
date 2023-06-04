b9f6e437b73b9164012535427b722a65e126b1c3
cheonnayeong <nayoung109191@gmail.com>
2023-06-04 23:22:23 +0900

    FEAT :: Notice

---
+++ src/components/Notice/Notice.js
@@ -0,0 +1,7 @@
+import * as S from "./style";
+
+function Notice() {
+  return <S.Notice></S.Notice>;
+}
+
+export default Notice;
---
+++ src/components/Notice/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Notice = styled.div`
+  width: 900px;
+  height: 300px;
+  background-color: #95b9ff;
+  border-radius: 10px;
+  margin: 0 auto; //상,우,하,좌
+`;
