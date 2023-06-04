3e5eb2e144c833fefd48438ed52ee0a472f8cd1a
cheonnayeong <nayoung109191@gmail.com>
2023-06-04 23:29:02 +0900

    CHORE :: Footer 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,7 @@ import * as S from "./style";
 function Footer() {
   return (
     <S.Footer>
-      <S.LeftText>
-        <div>부산소마고의 유일한 상남자들</div>
-        <div>@I-F</div>
-      </S.LeftText>
-      <S.LightText>copyright in 2023</S.LightText>
+      <p>소마월드에 오신 것을 환영합니다~!</p>
     </S.Footer>
   );
 }
