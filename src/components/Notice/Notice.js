3e9aeca3ddd8a058308bd76b66d13509a0d81904
cheonnayeong <nayoung109191@gmail.com>
2023-06-06 21:48:41 +0900

    두개재~

--- src/components/Notice/Notice.js
+++ src/components/Notice/Notice.js
@@ -1,11 +1,19 @@
 import * as S from "./style";
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< HEAD
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 
 function Notice() {
   return <S.Notice></S.Notice>;
 }
 
 export default Notice;
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
 // import arrow from "./assets/arrow.png";
 
@@ -15,3 +23,7 @@ function NoticeBox() {
 
 export default NoticeBox;
 >>>>>>> b426e45b8de02d45275ab40cd8f8a8604a5d0945
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
