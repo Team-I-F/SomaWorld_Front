52b4de01172a8d855150321f4cfba48d01d9d981
cheonnayeong <nayoung109191@gmail.com>
2023-06-06 21:50:05 +0900

    뭔가 문제가 있음

--- src/components/Notice/Notice.js
+++ src/components/Notice/Notice.js
@@ -1,29 +1,7 @@
 import * as S from "./style";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 function Notice() {
   return <S.Notice></S.Notice>;
 }
 
 export default Notice;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-// import arrow from "./assets/arrow.png";
-
-function NoticeBox() {
-  return <div>{/* <img src={arrow} alt="아" /> */}</div>;
-}
-
-export default NoticeBox;
->>>>>>> b426e45b8de02d45275ab40cd8f8a8604a5d0945
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
--- src/components/Notice/style.js
+++ src/components/Notice/style.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-<<<<<<< HEAD
 
 export const Notice = styled.div`
   width: 900px;
@@ -8,5 +7,3 @@ export const Notice = styled.div`
   border-radius: 10px;
   margin: 0 auto; //상,우,하,좌
 `;
-=======
->>>>>>> b426e45b8de02d45275ab40cd8f8a8604a5d0945
