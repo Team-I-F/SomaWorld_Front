08e1980843e83558471d819d660fb136ebfb2019
cheonnayeong <nayoung109191@gmail.com>
2023-06-15 16:40:35 +0900

    CHORE :: main 페이지 전체 수정

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -7,12 +7,19 @@ const Post = () => {
       <S.BottomBox>
         <S.Texts>
           <S.Title>심지혜가 자퇴를???</S.Title>
+
           <S.Script>
             2023년 5월 심지혜씨가 돌연 자퇴를 선언했다. 남은 천나영씨는 홀로
             디자인 시스템과 프론트를 도맏게 되었다
           </S.Script>
-          <S.Nickname>oo</S.Nickname>
-          <S.view>6</S.view>
+
+          <div style={{ display: "flex" }}>
+            <S.Nickname>oo</S.Nickname>
+            <S.view>
+              <div style={{ marginRight: "5px" }}>6</div>
+              <S.like src="assets/img4.png" type="button"></S.like>
+            </S.view>
+          </div>
         </S.Texts>
       </S.BottomBox>
     </S.PostTap>
--- src/pages/Main/style.js
+++ src/pages/Main/style.js
@@ -16,7 +16,7 @@ export const posts = styled.div`
 
 export const PostTabTitle = styled.p`
   font-size: 30px;
-  font-family: "MBC1961GulimM";
+  font-family: "MBC1961GulimM", cursive;
   display: flex;
   margin-left: 8%;
 `;
