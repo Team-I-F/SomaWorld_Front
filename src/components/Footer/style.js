11d7a9c9b173ec144b75ca6baa3f5ff8bed3a865
Wickhye <jihyesim170@gmail.com>
2023-06-15 16:46:35 +0900

    STYLE :: 푸터 고정

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 export const Footer = styled.div`
+  position: sticky;
+  bottom: 0;
+
   background-color: #95b9ff;
   height: 40px;
   width: 100%;
-  position: absolute;
-  margin-top: 50px;
-  color: white;
 
+  color: white;
   display: flex;
   align-items: center;
 `;
