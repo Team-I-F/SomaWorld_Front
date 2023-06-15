9f0ec44b37e46f26cffbcf79702096796b4e1a13
Wickhye <jihyesim170@gmail.com>
2023-06-15 17:16:09 +0900

    STYLE :: 폰트 적용

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,9 @@ function Footer() {
   return (
     <S.Footer>
       <S.FooterImg src="assets/img8.png"></S.FooterImg>
-      <span>소마월드에 오신 것을 환영합니다~!</span>
+      <span style={{ fontWeight: "550" }}>
+        소마월드에 오신 것을 환영합니다~!
+      </span>
     </S.Footer>
   );
 }
---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: "Cafe24Ssurround";
+  }
+
+  body {
+    font-family: "Helvetica", "Arial", sans-serif;
+    line-height: 1.5;
+  }
+
+`;
+
+export default GlobalStyle;
