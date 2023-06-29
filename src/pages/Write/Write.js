a23896d52e6f20fc2fd7ad8e4a5b4096a2a22e56
Wickhye <jihyesim170@gmail.com>
2023-06-29 17:38:26 +0900

    CHORE :: 데이터 깔끔하게 받아옴

--- src/pages/Write/Write.js
+++ src/pages/Write/Write.js
@@ -1,47 +1,62 @@
 import React, { useState } from "react";
 import { createPost } from "../../utils/api/board";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const PostForm = () => {
   const { boardID } = useParams();
+  const navigate = useNavigate();
 
-  const [title, setTitle] = useState("");
-  const [userNickname, setUserNickname] = useState("");
-  const [description, setDescription] = useState("");
+  const [formData, setFormData] = useState({
+    title: "",
+    userNickname: "",
+    description: "",
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const data = {
         tableInfoId: boardID,
-        title,
-        userNickname,
-        description,
+        ...formData,
       };
       const createdPost = await createPost(data);
       console.log("게시물 작성 완료:", createdPost);
+
+      // 아이 이동
+      // navigate("/about");
     } catch (error) {
       console.log("게시물 작성 실패:", error.message);
     }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input
         type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
+        name="title"
+        value={formData.title}
+        onChange={handleInputChange}
         placeholder="제목"
       />
       <input
         type="text"
-        value={userNickname}
-        onChange={(e) => setUserNickname(e.target.value)}
+        name="userNickname"
+        value={formData.userNickname}
+        onChange={handleInputChange}
         placeholder="작성자"
       />
       <textarea
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
+        name="description"
+        value={formData.description}
+        onChange={handleInputChange}
         placeholder="본문"
       ></textarea>
       <button type="submit">작성</button>
