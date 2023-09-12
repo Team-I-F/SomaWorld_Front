826768e444a2a5afca661087b9b433667d1116e9
Wickhye <jihyesim170@gmail.com>
2023-09-12 23:18:34 +0900

    STYLE : 로그인 하기 전 메인

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,83 @@
 import Button from "./Button/Button";
-import * as S from "./style";
+import styled from "styled-components";
 
 function Header() {
   return (
     <div>
-      <S.Head style={{margin: 'auto 200px'}}>
-        <S.HeadTitle>SomaWorld</S.HeadTitle>
+      <HeadBox>
+        <BokBut>
 
-        <S.Buttons>
-          <S.SearchButton type="button" src="assets/img.png"></S.SearchButton>
+        <HeadTitle>SomaWorld</HeadTitle>
+
+        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+          
+          {/* 로그인 안한 경우 */}
+
+          <Button name={"회원가입"} />
+          <Button name={"로그인"}/>
+
+
+          {/* 로그인 한 경우 */}
+          
+          {/* <Search>
+            <SearchInput type="text" placeholder="검색어 입력" />
+            <SearchImg src="assets/img.png" style={{color: 'red'}}/>
+          </Search> */}
+
+        </div>
+
+        </BokBut>
+
+
+        {/* <Buttons>
+          <SearchButton type="button" src="assets/img.png"></SearchButton>
           <Button name={"로그인"}/>
           <Button name={"회원가입"} />
-        </S.Buttons>
-      </S.Head>
-      <S.line></S.line>
+        </Buttons> */}
+      </HeadBox>
+      <line></line>
     </div>
   );
 }
 
 export default Header;
+
+const BokBut = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: auto 200px;
+`
+
+const HeadBox = styled.div`
+  padding: 10px;
+  border-bottom: 1px solid #B0B0B0;
+`;
+
+const HeadTitle = styled.span`
+  font-size: 35px;
+  line-height: 44px;
+  font-style: normal;
+  margin: 5px 0px 0px 40px;
+  font-weight: bold;
+  font-family: Cafe24Ssurround;
+`;
+
+const Search = styled.div`
+  position: relative;
+  width: 200px;
+  background-color: antiquewhite;
+`;
+
+const SearchInput = styled.div`
+  width: 100%;
+  border: 1px solid #bbb;
+  border-radius: 8px;
+  padding: 10px 12px;
+  font-size: 14px;
+
+`;
+
+const SearchImg = styled.div`
+  background-color: aliceblue;
+`;
