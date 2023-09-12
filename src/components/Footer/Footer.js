3ee0783cf43a37e535945ce2b193a888669ba7ba
Wickhye <jihyesim170@gmail.com>
2023-09-12 20:31:09 +0900

    STYLE : 헤더 푸터 마진 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,13 @@ import * as S from "./style";
 function Footer() {
   return (
     <S.Footer>
-      <S.FooterImg src="assets/img8.png"></S.FooterImg>
-      <span style={{ fontWeight: "550" }}>
-        소마월드에 오신 것을 환영합니다~!
-      </span>
+      <div style={{marginLeft: '250px'}}>
+        <S.FooterImg src="assets/img8.png"></S.FooterImg>
+        <span style={{ fontWeight: "550" }}>
+          소마월드에 오신 것을 환영합니다~!
+        </span>
+      </div>
+      
     </S.Footer>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,12 @@ import * as S from "./style";
 function Header() {
   return (
     <div>
-      <S.Head>
+      <S.Head style={{margin: 'auto 200px'}}>
         <S.HeadTitle>SomaWorld</S.HeadTitle>
 
         <S.Buttons>
           <S.SearchButton type="button" src="assets/img.png"></S.SearchButton>
-          <Button name={"로그인"} />
+          <Button name={"로그인"}/>
           <Button name={"회원가입"} />
         </S.Buttons>
       </S.Head>
