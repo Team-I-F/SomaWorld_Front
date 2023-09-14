2235f57b1385a98b811e719d4ccf2f5e0d9804ae
Wickhye <jihyesim170@gmail.com>
2023-09-14 13:57:33 +0900

    ADD : 검색 바

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,35 +2,16 @@ import { Link, useNavigate } from "react-router-dom";
 import Button from "./Button/Button";
 import styled from "styled-components";
 import { useState } from "react";
+import SearchBar from "./SearchBar/SearchBar";
 
 function Header() {
-  const navigate = useNavigate();
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    navigate(`/search?term=${searchTerm}`);
-  };
-
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
   return (
     <HeadBox>
       <HeadInBox>
         <HeadTitle>SomaWorld</HeadTitle>
-        <form onSubmit={handleSubmit}>
-          <Search>
-            <SearchImg src="assets/img.png" />
-            <SearchInput
-              type="text"
-              value={searchTerm}
-              onChange={handleInputChange}
-              placeholder="검색어를 입력하세요..."
-            />
-          </Search>
-        </form>
+
+        <SearchBar/>    
+
 
         <div style={{ display: "flex", alignItems: "center" }}>
           {/* 로그인 안한 경우 */}
@@ -71,26 +52,4 @@ const HeadTitle = styled.span`
   font-size: 35px;
   line-height: 44px;
   font-weight: bold;
-`;
-
-const Search = styled.div`
-  position: relative;
-  display: flex;
-  width: 550px;
-  border-radius: 100px;
-  padding: 7px 5px;
-  background-color: #f3f3f3;
-`;
-
-const SearchInput = styled.input`
-  width: 100%;
-  border: none;
-  font-size: 14px;
-  outline: none;
-  background-color: #f3f3f3;
-`;
-
-const SearchImg = styled.img`
-  width: 25px;
-  height: 25px;
 `;
